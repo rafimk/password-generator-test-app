ee388247737271fc573ef9fac14d0394940ca8e5
Muhammed Rafi <m.rafi@afad.gov.ae>
2022-09-06 09:12:20 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using PasswordGenerator;
+using password_generator_test_app;
+
+var pwdPrefix = new Password().IncludeUppercase().LengthRequired(2);
+var pwdSuffix = new Password().IncludeNumeric().LengthRequired(6);
+RandomGenerator randomGenerator = new RandomGenerator();
+var prefix = "";
+do 
+{
+    prefix = randomGenerator.RandomString(2);
+} while (prefix.Contains("I") && prefix.Contains("O"));
+var password = randomGenerator.RandomString(2, false) + pwdSuffix.Next();
+Console.WriteLine($"New password is {password}");
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"# password-generator-test-app" 
